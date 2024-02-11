c2f027ed32033fa08a07d706016f10b00fd4f7c3
David Jaja <jajadavidjid@gmail.com>
2024-02-11 12:58:49 +0100

    Second commit

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 // index.js
 const box = document.querySelector(".box");
 
-gsap.to(box, {
-  y: 45, // Bounce down by 100 pixels
-  duration: 0.5, // Animation duration
-  ease: "power2.inOut", // Bounce easing
-  yoyo: true, // Bounce back up
-  repeat: -1, // Repeat indefinitely
-});
+// gsap.to(box, {
+//   y: 45, // Bounce down by 100 pixels
+//   duration: 0.5, // Animation duration
+//   ease: "power2.inOut", // Bounce easing
+//   yoyo: true, // Bounce back up
+//   repeat: -1, // Repeat indefinitely
+// });
 
 // anime({
 //   targets: box,
@@ -23,16 +23,29 @@ gsap.to(box, {
 // // index.js
 // const box = document.querySelector(".box");
 
-// Velocity(
-//   box,
-//   {
-//     translateY: 45, // Bounce down by 45 pixels
-//   },
-//   {
-//     duration: 500, // Animation duration in milliseconds
-//     easing: "easeInOutQuad", // Bounce easing
-//     loop: true, // Repeat indefinitely
-//   }
-// );
+Velocity(
+  box,
+  {
+    bottom: 45,
+  },
+  {
+    duration: 500,
+    easing: "easeInOutQuad",
+    loop: true,
+  }
+);
 
-// console.log(Velocity);
+console.log(Velocity);
+
+// function animateElement() {
+//   // const element = document.getElementById("greeting");
+
+//   box.velocity({ left: "200px" }, { loop: true });
+// }
+
+// // Call the function when the page loads
+// document.addEventListener("DOMContentLoaded", animateElement);
+
+// box.addEventListener("click", () => {
+//   box.velocity({ left: "200px" }, { loop: true });
+// });
